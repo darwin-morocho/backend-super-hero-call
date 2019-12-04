249917855cf8384a5dea4bde9dff5228c7690ecc
Darwin Morocho <darwin@MacBook-Pro-de-Darwin.local>
2019-12-03 20:56:00 -0500

    added axios

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 import http from "http";
 import io from "socket.io";
+import axios from "axios";
 import dotenv from "dotenv";
 import ws from "./ws";
 dotenv.config();
@@ -13,4 +14,8 @@ ws(io(server));
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`running on ${PORT}`);
+  //every 5 minutes
+  setInterval(() => {
+    axios.get("https://backend-super-hero-call.herokuapp.com");
+  }, 1000 * 60 * 5);
 });
